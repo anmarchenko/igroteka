0b9a57ec2fab2dc8d6b27dcf19950aabce0d744a
Andrey Marchenko <igendou@gmail.com>
2019-11-07 14:25:19 +0100

    miagrate scores

---
+++ priv/repo/migrations/20191107125727_migrate_scores.exs
@@ -0,0 +1,42 @@
+defmodule Skaro.Repo.Migrations.MigrateScores do
+  use Ecto.Migration
+
+  alias Skaro.Backlog.Entry
+  alias Skaro.Repo
+
+  def change do
+    Entry
+    |> Repo.all()
+    |> Enum.each(fn entry ->
+      {:ok, _} =
+        entry
+        |> Entry.update(%{score: new_score(entry.score)})
+        |> Repo.update()
+    end)
+  end
+
+  defp new_score(old_score) do
+    case old_score do
+      nil ->
+        nil
+
+      0 ->
+        0
+
+      10 ->
+        5
+
+      9 ->
+        4
+
+      8 ->
+        3
+
+      x when x > 5 && x < 8 ->
+        2
+
+      _ ->
+        1
+    end
+  end
+end
