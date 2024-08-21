6ff479558904ab6a13c57803d391ae6ea1e5645b
Andrey Marchenko <andrei.marchenko@protonmail.com>
2024-08-21 09:18:02 +0200

    migrate from deprecated gettext usage

--- lib/skaro_web/gettext.ex
+++ lib/skaro_web/gettext.ex
@@ -5,7 +5,7 @@ defmodule SkaroWeb.Gettext do
   By using [Gettext](https://hexdocs.pm/gettext),
   your module gains a set of macros for translations, for example:
 
-      import SkaroWeb.Gettext
+      use Gettext, backend: SkaroWeb.Gettext
 
       # Simple translation
       gettext("Here is the string to translate")
@@ -20,5 +20,5 @@ defmodule SkaroWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :skaro
+  use Gettext.Backend, otp_app: :skaro
 end
