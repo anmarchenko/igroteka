7214255c2aba8307beda991f8db53e5ebcddcb36
Andrey Marchenko <andrei.marchenko@protonmail.com>
2024-10-13 18:51:38 +0200

    opencritic client tracing

--- lib/skaro/opencritic/client.ex
+++ lib/skaro/opencritic/client.ex
@@ -5,98 +5,109 @@ defmodule Skaro.Opencritic.Client do
 
   alias Skaro.{HttpClient, Parser}
   alias Skaro.Opencritic.Mapper
-  alias Skaro.Tracing
 
-  @event_call [:skaro, :opencritic, :call]
-  @event_error [:skaro, :opencritic, :error]
+  require OpenTelemetry.Tracer, as: Tracer
 
   def find(%{name: nil}), do: {:error, "name is not given"}
 
   def find(%{name: name}) do
-    case search(name) do
-      {:ok, game_id} ->
-        get_by_id(game_id)
+    Tracer.with_span "opencritic.client.find", kind: :client, attributes: %{game_name: name} do
+      case search(name) do
+        {:ok, game_id} ->
+          get_by_id(game_id)
 
-      {:error, :not_found} ->
-        record_error(:not_found)
-        {:error, :not_found}
+        {:error, :not_found} ->
+          {:error, :not_found}
 
-      {:error, _} = error_tuple ->
-        record_error(:other)
-        error_tuple
+        {:error, _} = error_tuple ->
+          error_tuple
+      end
     end
   end
 
   def find(_), do: {:error, "argument is invalid"}
 
   def get_by_id(game_id) do
-    trace(
-      :get_by_id,
-      fn ->
-        with body when is_binary(body) <- HttpClient.get(game_url(game_id), %{}, headers()),
-             {:ok, %{"numTopCriticReviews" => num} = game_json} when num > 0 <-
-               Parser.parse_json(body),
-             {:ok, cover_reviews} <- get_cover_reviews(game_id) do
-          {:ok,
-           Map.merge(
-             %{
-               external_id: Integer.to_string(game_json["id"]),
-               tier: game_json["tier"],
-               percent_recommended: game_json["percentRecommended"],
-               score: game_json["topCriticScore"],
-               num_reviews: game_json["numTopCriticReviews"]
-             },
-             cover_reviews
-           )}
-        else
-          {:error, _} = error_tuple ->
-            record_error(:other)
-            error_tuple
+    Tracer.with_span "opencritic.client.get_by_id", kind: :client, attributes: %{game_id: game_id} do
+      with body when is_binary(body) <- HttpClient.get(game_url(game_id), %{}, headers()),
+           {:ok, %{"numTopCriticReviews" => num} = game_json} when num > 0 <-
+             Parser.parse_json(body),
+           {:ok, cover_reviews} <- get_cover_reviews(game_id) do
+        Tracer.set_attribute(:result, :ok)
 
-          _ ->
-            record_error(:not_found)
-            {:error, :not_found}
-        end
+        {:ok,
+         Map.merge(
+           %{
+             external_id: Integer.to_string(game_json["id"]),
+             tier: game_json["tier"],
+             percent_recommended: game_json["percentRecommended"],
+             score: game_json["topCriticScore"],
+             num_reviews: game_json["numTopCriticReviews"]
+           },
+           cover_reviews
+         )}
+      else
+        {:error, reason} = error_tuple ->
+          Tracer.set_attribute(:result, :external_api_failure)
+
+          Tracer.set_status(OpenTelemetry.status(:error, "OpenCritic API call failed: #{reason}"))
+
+          error_tuple
+
+        _ ->
+          Tracer.set_attribute(:result, :opencritic_reviews_not_found)
+
+          {:error, :not_found}
       end
-    )
+    end
   end
 
   def get_cover_reviews(game_id) do
-    trace(
-      :get_cover_reviews,
-      fn ->
-        with body when is_binary(body) <- HttpClient.get(cover_url(game_id), %{}, headers()),
-             {:ok, reviews} <- Parser.parse_json(body) do
-          {:ok,
-           %{
-             reviews: Mapper.reviews(reviews)
-           }}
-        else
-          {:error, _} = error_tuple ->
-            error_tuple
-        end
+    Tracer.with_span "opencritic.client.get_cover_reviews",
+      kind: :client,
+      attributes: %{game_id: game_id} do
+      with body when is_binary(body) <- HttpClient.get(cover_url(game_id), %{}, headers()),
+           {:ok, reviews} <- Parser.parse_json(body) do
+        Tracer.set_attribute(:result, :ok)
+
+        {:ok,
+         %{
+           reviews: Mapper.reviews(reviews)
+         }}
+      else
+        {:error, reason} = error_tuple ->
+          Tracer.set_attribute(:result, :external_api_failure)
+
+          Tracer.set_status(OpenTelemetry.status(:error, "OpenCritic API call failed: #{reason}"))
+
+          error_tuple
       end
-    )
+    end
   end
 
   defp search(name) do
-    trace(
-      :search,
-      fn ->
-        with body when is_binary(body) <-
-               HttpClient.get(search_url(), %{criteria: name}, headers()),
-             {:ok, [%{"dist" => dist, "id" => game_id} | _]} when dist < 0.1 <-
-               Parser.parse_json(body) do
-          {:ok, game_id}
-        else
-          {:error, _} = error_tuple ->
-            error_tuple
+    Tracer.with_span "opencritic.client.search", kind: :client, attributes: %{game_name: name} do
+      with body when is_binary(body) <-
+             HttpClient.get(search_url(), %{criteria: name}, headers()),
+           {:ok, [%{"dist" => dist, "id" => game_id} | _]} when dist < 0.1 <-
+             Parser.parse_json(body) do
+        Tracer.set_attribute(:result, :ok)
 
-          _ ->
-            {:error, :not_found}
-        end
+        {:ok, game_id}
+      else
+        {:error, reason} = error_tuple ->
+          Tracer.set_attribute(:result, :external_api_failure)
+
+          Tracer.set_status(OpenTelemetry.status(:error, "OpenCritic API call failed: #{reason}"))
+
+          error_tuple
+
+        _ ->
+          Tracer.set_attribute(:result, :game_not_found_in_opencritic)
+
+          {:error, :not_found}
       end
-    )
+    end
   end
 
   defp search_url, do: "#{base_url()}/game/search"
@@ -112,12 +123,4 @@ defmodule Skaro.Opencritic.Client do
   defp base_url, do: Application.fetch_env!(:skaro, :opencritic)[:base_url]
 
   defp api_key, do: Application.fetch_env!(:skaro, :opencritic)[:api_key]
-
-  defp trace(action, fun) do
-    Tracing.trace(@event_call, %{action: action}, fun)
-  end
-
-  defp record_error(reason) do
-    Tracing.send_count(@event_error, 1, %{reason: reason})
-  end
 end
